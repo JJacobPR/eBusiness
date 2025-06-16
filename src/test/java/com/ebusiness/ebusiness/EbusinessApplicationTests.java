8ef125d49130cce5636ac58b26aa26a1e9594b94
Jakub Janicki <jjanicki08@gmail.com>
2025-06-16 15:48:32 +0200

    Initial Commit

---
+++ src/test/java/com/ebusiness/ebusiness/EbusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebusiness.ebusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
